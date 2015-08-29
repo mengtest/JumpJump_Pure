588dbfa31661769f39920876177086555b2cd25f
wangyankai <wuxikq@gmail.com>
2015-08-29 11:49:02 +0800

    Game Data 实例化

--- JumpJump/Assets/MAssets/Scripts/GameData/GameData.cs
+++ JumpJump/Assets/MAssets/Scripts/GameData/GameData.cs
@@ -2,7 +2,66 @@
 using System.Collections;
 
 public class GameData  {
-	SettingData settingData;
-	PerpetualData PerpetualDataData;
-	RunningData runningData;
+
+
+	public static GameData Instance {
+		get{
+			if(_Instance == null){
+				_Instance = new GameData();
+			}
+			return _Instance;
+		}
+		set
+		{
+			_Instance= value;
+		}
+	}
+	
+	private static GameData _Instance;
+
+	public SettingData MSettingData {
+		get{
+			if(_MSettingData == null){
+				_MSettingData = new SettingData();
+			}
+			return _MSettingData;
+		}
+		set
+		{
+			_MSettingData= value;
+		}
+	}
+	private SettingData _MSettingData;
+
+
+	public PerpetualData MPerpetualData {
+		get{
+			if(_MPerpetualData == null){
+				_MPerpetualData = new PerpetualData();
+			}
+			return _MPerpetualData;
+		}
+		set
+		{
+			_MPerpetualData= value;
+		}
+	}
+	private PerpetualData _MPerpetualData;
+
+
+
+	public RunningData MRunningData {
+		get{
+			if(_MRunningData == null){
+				_MRunningData = new RunningData();
+			}
+			return _MRunningData;
+		}
+		set
+		{
+			_MRunningData= value;
+		}
+	}
+	private RunningData _MRunningData;
+
 }

e42ab0ca7dff22130cde552b4ce243e451cda01c
XuSheng <xusheng@doodlemobile.comls>
2015-11-02 19:35:05 +0800

    1.调整了时间的响应处理 2.调整了球的物理材质

--- JumpJump/Assets/MAssets/Scripts/PlayScene/PlayerController.cs
+++ JumpJump/Assets/MAssets/Scripts/PlayScene/PlayerController.cs
@@ -28,6 +28,8 @@ public class PlayerController : MonoBehaviour
 
 		Physics.gravity = m_Gravity;
 		m_InitMoveSpeed = moveSpeed;
+
+		m_sphereCollider.material = Resources.Load ("PhysicMaterial/Ice") as PhysicMaterial;
 	}
 	
 	// Update is called once per frame
@@ -41,20 +43,26 @@ public class PlayerController : MonoBehaviour
 	{
 
 
-		Vector3 velocityChange = Vector3.right * moveSpeed.x - m_rigidbody.velocity;
+		Vector3 velocityChange = Vector3.right * GetMoveSpeed () - m_rigidbody.velocity;
 		velocityChange.y = 0;
 		m_rigidbody.AddForce (velocityChange, ForceMode.VelocityChange);
 
 
-//		m_OnGround = false;
+		m_OnGround = false;
+	}
+
+	float GetMoveSpeed ()
+	{
+		return m_OnGround ? moveSpeed.x : moveSpeed.x * 0.9f;
 	}
 
 	void OnCollisionEnter (Collision collsion)
 	{	
 		CheckFaild (collsion);
 		m_OnGround = true;
-		m_JumpTimes = 0;
+//		m_JumpTimes = 0;
 		OnFunction (collsion);
+//		m_JumpValidateTime=0;
 
 	}
 
@@ -62,6 +70,7 @@ public class PlayerController : MonoBehaviour
 	{
 		CheckFaild (collsion);
 		m_OnGround = true;
+//		m_JumpValidateTime=0;
 	}
 
 	void OnCollisionExit (Collision collsion)
@@ -70,13 +79,21 @@ public class PlayerController : MonoBehaviour
 		if (brick != null) {
 			brick.M_IsPlayerCollideExit = true;
 		}
+//		m_OnGround=false;
 	}
+
+	float m_JumpValidateTime = 0;
 	
 	public void OnTouchDown ()
 	{
-		if (m_JumpTimes < m_MaxJumpTimes) {
 
-			m_rigidbody.AddForce (Vector3.up * moveSpeed.y, ForceMode.VelocityChange);
+		if (m_JumpTimes < m_MaxJumpTimes) { 
+			if (m_JumpTimes == 0 && (m_JumpValidateTime += Time.deltaTime) > 0.03f) {
+				return;
+			}
+			Vector3 v = m_rigidbody.velocity;
+			v.y = moveSpeed.y;
+			m_rigidbody.velocity = v;
 			m_JumpTimes++;
 		}
 	}
@@ -85,20 +102,70 @@ public class PlayerController : MonoBehaviour
 
 	void CheckGrounded ()
 	{
-		ray.origin = m_sphereCollider.bounds.center;
+		Vector3 origin = m_sphereCollider.bounds.center;
+		ray.origin = origin;
 		ray.direction = Vector3.down;
-		float dst = m_sphereCollider.radius + 0.2f;
-		if (Physics.Raycast (ray, dst)) {
-			m_OnGround = true;
+		float dst = m_sphereCollider.radius + 0.05f;
+//		if (Physics.Raycast (ray, dst) && m_rigidbody.velocity.y<0 ) {
 //			m_JumpTimes = 0;
-		} else {
-			m_OnGround = false;
+//		} else {
+//		}
+
+
+		Debug.DrawLine (ray.origin, ray.origin + ray.direction * dst, Color.red);
+
+
+		if (m_rigidbody.velocity.y > 0)
+			return;
+
+		int lay = 1 << LayerMask.NameToLayer ("Player");
+
+		RaycastHit hit;
+		bool check1 = Physics.Raycast (ray, out hit, dst, ~lay);
+//		if (check1) {
+//			m_JumpTimes = 0;
+//			m_JumpValidateTime = 0;
+//			Debug.Log (" hit pot=" + hit.point + " hit name=" + hit.collider.name + " m_rigidbody.velocity.y=" + m_rigidbody.velocity.y);
+//		}
+
+		origin.x=m_sphereCollider.bounds.center.x+m_sphereCollider.radius;
+		ray.origin=origin;
+		bool check2 = Physics.Raycast (ray, out hit, dst, ~lay);
+//		if (check2) {
+//			m_JumpTimes = 0;
+//			m_JumpValidateTime = 0;
+//			Debug.Log (" hit pot=" + hit.point + " hit name=" + hit.collider.name + " m_rigidbody.velocity.y=" + m_rigidbody.velocity.y);
+//		}
+		origin.x=m_sphereCollider.bounds.center.x+m_sphereCollider.radius;
+				ray.origin=origin;
+		bool check3 = Physics.Raycast (ray, out hit, dst, ~lay);
+		if (check1 || check2 || check3) {
+			m_JumpTimes = 0;
+			m_JumpValidateTime = 0;
+//			Debug.Log (" hit pot=" + hit.point + " hit name=" + hit.collider.name + " m_rigidbody.velocity.y=" + m_rigidbody.velocity.y);
 		}
+
+
+
+
+//		bool check1=Physics.Raycast(ray,dst,~lay);
+//		origin.x=m_sphereCollider.bounds.center.x-m_sphereCollider.radius;
+//		ray.origin=origin;
+//		bool check2=Physics.Raycast(ray,dst,~lay);
+//		origin.x=m_sphereCollider.bounds.center.x+m_sphereCollider.radius;
+//		ray.origin=origin;
+//		bool check3=Physics.Raycast(ray,dst,~lay);
+//
+//		if(check1 || check2 || check3){
+//			m_JumpTimes=0;
+//			m_JumpValidateTime=0; Debug.Log(" ******** dst="+dst+" ray.origion"+ray.origin);
+//		}
+
 	}
 
 	void CheckFaild (Collision collision)
 	{
-		float threasholdHeight = m_sphereCollider.bounds.center.y - m_sphereCollider.radius * 0.5f;
+		float threasholdHeight = m_sphereCollider.bounds.center.y - m_sphereCollider.radius * 0.4f;
 		
 		for (int i=0; i<collision.contacts.Length; i++) {
 			ContactPoint cp = collision.contacts [i]; 
@@ -116,7 +183,7 @@ public class PlayerController : MonoBehaviour
 	
 	void CheckFailed ()
 	{
-		if (transform.position.y < 0) {
+		if (transform.position.y < -0.1f) {
 			OnFaild ();
 		}
 	}
@@ -126,7 +193,11 @@ public class PlayerController : MonoBehaviour
 		transform.position = m_InitPot;
 		transform.rotation = Quaternion.Euler (m_InitRotate);
 		m_OnGround = false;
-		SetEmptyFunction();
+		SetEmptyFunction ();
+		m_JumpTimes = 0;
+		m_JumpValidateTime = 0;
+		m_rigidbody.velocity = Vector3.zero;
+
 
 	}
 

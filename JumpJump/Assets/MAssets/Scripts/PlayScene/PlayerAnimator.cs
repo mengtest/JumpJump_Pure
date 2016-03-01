90d55d7c695ff2e3ea5940a65de32915eb8be0a0
wangyankai <wuxikq@gmail.com>
2016-03-01 21:36:19 +0800

    动画播放接口加上

---
+++ JumpJump/Assets/MAssets/Scripts/PlayScene/PlayerAnimator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimator : MonoBehaviour
+{
+
+	public static readonly string PLAYER_STATE = "PlayerState";
+	public static readonly string SAME_STATE = "SameState";
+	private Animator animator;
+	AnimatorState lastState;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+//		AnimatorClipInfo[] animatorClipInfos = animator.GetCurrentAnimatorClipInfo (0);
+//		foreach (AnimatorClipInfo animatorClipInofo in animatorClipInfos) {
+////			if(animatorClipInofo.clip.name.Equals("Run")){
+//			AnimationEvent animationEvent = new AnimationEvent();
+//			animationEvent.functionName = "SetSameTrue";
+//			animationEvent.time = 0.1f;
+//			animatorClipInofo.clip.AddEvent(animationEvent);
+////			}
+//		}
+		lastState = AnimatorState.Idle;
+
+//		PlayAnimation (AnimatorState.Run);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void PlayAnimation (AnimatorState animatorState)
+	{
+
+		if (lastState != animatorState) {
+			animator.SetFloat (PLAYER_STATE, (int)animatorState);
+			animator.SetBool (SAME_STATE, false);
+			lastState = animatorState;
+			StartCoroutine (SetSameTrue ());
+		}
+	}
+
+
+	public enum AnimatorState
+	{
+		Idle=0,
+		Run=1,
+		Jump_up=2,
+		Jump_down=3,
+		Jump_onGround=4
+	}
+
+	IEnumerator SetSameTrue ()
+	{
+		yield return new WaitForSeconds (0.1f);
+		animator.SetBool (SAME_STATE, true);
+	}
+
+}

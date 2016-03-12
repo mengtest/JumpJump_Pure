a5b879c2f3484dc2ce54ac5922b3b4ba8d1c5cc4
XuSheng <xusheng@doodlemobile.comls>
2016-03-12 15:15:08 +0800

    1.update cplayerstate

--- JumpJump/Assets/MAssets/Scripts/PlayScene/CplayerState.cs
+++ JumpJump/Assets/MAssets/Scripts/PlayScene/CplayerState.cs
@@ -56,30 +56,85 @@ public class Run_State :State<CPlayer>
 }
 #endregion
 
-#region Jump
-public class Jump_State :State<CPlayer>
+#region Jump_Up
+public class Jump_Up_State :State<CPlayer>
 {
 	public override void Execute (CPlayer player)
 	{
-		player.Jump_Execute ();
+		player.Jump_Up_Execute ();
 	}
 	
 	public override void Enter (CPlayer player)
 	{
-		player.Jump_Enter ();
+		player.Jump_Up_Enter ();
 	}
 	
 	public override void Exit (CPlayer player)
 	{
-		player.Jump_Exit ();
+		player.Jump_Up_Exit ();
 	}
 	
 	public override string ToString ()
 	{
-		return string.Format ("[Jump_State]");
+		return string.Format ("[Jump_Up_State]");
 	}
 }
 
 #endregion
+
+
+#region Jump_Down
+public class Jump_Down_State :State<CPlayer>
+{
+	public override void Execute (CPlayer player)
+	{
+		player.Jump_Down_Execute ();
+	}
+	
+	public override void Enter (CPlayer player)
+	{
+		player.Jump_Down_Enter ();
+	}
+	
+	public override void Exit (CPlayer player)
+	{
+		player.Jump_Down_Exit ();
+	}
+	
+	public override string ToString ()
+	{
+		return string.Format ("[Jump_Down_State]");
+	}
+}
+
+#endregion
+
+
+#region jump_OnGround
+public class Jump_OnGround_State :State<CPlayer>
+{
+	public override void Execute (CPlayer player)
+	{
+		player.Jump_OnGround_Execute ();
+	}
+	
+	public override void Enter (CPlayer player)
+	{
+		player.Jump_OnGround_Enter ();
+	}
+	
+	public override void Exit (CPlayer player)
+	{
+		player.Jump_OnGround_Exit ();
+	}
+	
+	public override string ToString ()
+	{
+		return string.Format ("[Jump_OnGround_State]");
+	}
+}
+
+#endregion
+
 
 

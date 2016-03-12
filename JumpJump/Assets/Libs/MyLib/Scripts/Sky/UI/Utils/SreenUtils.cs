6047cf14327f2513b53bd429a36aba378c68de26
wangyankai <wuxikq@gmail.com>
2016-03-12 12:54:13 +0800

    代码优化

--- JumpJump/Assets/Libs/MyLib/Scripts/Sky/UI/Utils/SreenUtils.cs
+++ JumpJump/Assets/Libs/MyLib/Scripts/Sky/UI/Utils/SreenUtils.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class SkySreenUtils : MonoBehaviour
+public class SreenUtils : MonoBehaviour
 {
 
     public enum ScreenSizeType

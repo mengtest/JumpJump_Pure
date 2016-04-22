cc8312e74599b383b084bbffebba0318d94dc34b
wangyankai <wuxikq@gmail.com>
2016-04-22 08:31:07 +0800

    视频广告Button

--- JumpJump/Assets/MAssets/Scripts/UI/MainPanel.cs
+++ JumpJump/Assets/MAssets/Scripts/UI/MainPanel.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class MainPanel : UIWindow {
-	public void Play(){
-		GameController.GetInstance ().GetPlayGameInstance().OnStart ();
-		GameController.GetInstance ().GetMainPanel().ShowOut ();
-		GameController.GetInstance ().GetPlayPanel().ShowIn ();
+using Supergood.Unity;
+using Supergood.Unity.Ad;
+
+public class MainPanel : UIWindow
+{
+
+	public Button VedioButton;
+
+	public void Play ()
+	{
+		GameController.GetInstance ().GetPlayGameInstance ().OnStart ();
+		GameController.GetInstance ().GetMainPanel ().ShowOut ();
+		GameController.GetInstance ().GetPlayPanel ().ShowIn ();
 		GameController.GetInstance ().GetPlayPanel ().Start ();
+	}
+
+	void Update ()
+	{
+		if (SGConfig.Instant.isLoad) {
+			VedioButton.interactable = AdManager.instant.VideoIsLoad ();
+		}
 	}
 
 }
